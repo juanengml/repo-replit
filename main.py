05431e65ae5618abec7788207cf3a2eec4aca88c
Sr Marinho <37448831+juanengml@users.noreply.github.com>
2021-07-05 20:40:48 +0000

    test other dag

--- main.py
+++ main.py
@@ -1,59 +1,61 @@
-from datetime import datetime
-from pathlib import Path
-from datetime import datetime as dt 
-import pandas as pd
-from airflow import DAG
-from airflow.operators.bash import BashOperator
-from airflow.operators.python import PythonOperator
+
 from datetime import timedelta
 
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.dummy import DummyOperator
+from airflow.utils.dates import days_ago
 
-default_args = {
-    'owner': 'airflow'
-   # 'retry_delay': timedelta(minutes=5),
+args = {
+    'owner': 'airflow',
 }
 
-dag = DAG(dag_id='request_endpoint_event_test',
-          default_args=default_args,
-          schedule_interval='*/5 * * * *',
-          dagrun_timeout=timedelta(seconds=5))
+with DAG(
+    dag_id='example_bash_operator',
+    default_args=args,
+    schedule_interval='0 0 * * *',
+    start_date=days_ago(2),
+    dagrun_timeout=timedelta(minutes=5),
+    tags=['example', 'example2'],
+    params={"example_key": "example_value"},
+) as dag:
 
-endpoint = "http://ec2-54-91-136-29.compute-1.amazonaws.com:8080/status"
+    run_this_last = DummyOperator(
+        task_id='run_this_last',
+    )
 
-fetch_events = BashOperator(
-    task_id="fetch_events",
-    bash_command=(
-        "mkdir -p /tmp/events && "
-        "curl {} > /tmp/evento.json && cat /tmp/evento.json ; ".format(endpoint)
-        
-       ),
+    # [START howto_operator_bash]
+    run_this = BashOperator(
+        task_id='run_after_loop',
+        bash_command='echo 1',
+    )
+    # [END howto_operator_bash]
+
+    run_this >> run_this_last
+
+    for i in range(3):
+        task = BashOperator(
+            task_id='runme_' + str(i),
+            bash_command='echo "{{ task_instance_key_str }}" && sleep 1',
+        )
+        task >> run_this
+
+    # [START howto_operator_bash_template]
+    also_run_this = BashOperator(
+        task_id='also_run_this',
+        bash_command='echo "run_id={{ run_id }} | dag_run={{ dag_run }}"',
+    )
+    # [END howto_operator_bash_template]
+    also_run_this >> run_this_last
+
+# [START howto_operator_bash_skip]
+this_will_skip = BashOperator(
+    task_id='this_will_skip',
+    bash_command='echo "hello world"; exit 99;',
     dag=dag,
 )
+# [END howto_operator_bash_skip]
+this_will_skip >> run_this_last
 
-
-def _trata_json(input_path, output_path):
-    """Calculates event statistics."""
-    import json
-     # Opening JSON file
-    f = open(input_path)
-    data = json.load(f)
-    status = data['appState']['yoloStatus']
-    f.close()
-    import csv
-    # open the file in the write mode
-    with open(output_path, 'w') as f:
-    # create the csv writer
-       writer = csv.writer(f)
-       print(status, dt.now())
-       writer.writerow(status)
-   
-
-
-trata_json = PythonOperator(
-    task_id="trata_json",
-    python_callable=_trata_json,
-    op_kwargs={"input_path": "/tmp/evento.json", "output_path": "/tmp/stats.csv"},
-    dag=dag,
-)
-
-fetch_events >> trata_json
+if __name__ == "__main__":
+    dag.cli()
